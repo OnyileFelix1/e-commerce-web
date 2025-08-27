5cda9e70ea852204d980ea78a7d15f091efdda94
Onyile Bassey Felix <basseyonyile20044@gmail.com>
2025-08-27 11:09:13 +0100

    Update country-delete.php

--- country-delete.php
+++ country-delete.php
@@ -1,7 +1,9 @@
 <?php require_once('header.php'); ?>
 
 <?php
-// Preventing the direct access of this page.
+
+
+	//Preventing direct accesss from this page
 if(!isset($_REQUEST['id'])) {
 	header('location: logout.php');
 	exit;
@@ -19,7 +21,7 @@ if(!isset($_REQUEST['id'])) {
 
 <?php
 
-	// Delete from tbl_country
+		//Delete from table country
 	$statement = $pdo->prepare("DELETE FROM tbl_country WHERE country_id=?");
 	$statement->execute(array($_REQUEST['id']));
 
